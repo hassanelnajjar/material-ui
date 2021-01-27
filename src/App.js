b814efff8e569897800738285201f68214f30203
hassanelnajjar <hassansamielnajjar@gmail.com>
2021-01-27 13:42:14 +0200

    fix spaces

--- src/App.js
+++ src/App.js
@@ -2,23 +2,23 @@ import MaterialUI from './components/MaterialUI';
 import AntD from './components/AntD';
 import { ContextProvide } from './components/Context';
 import {
-	BrowserRouter as Router,
-	Route,
-	Switch,
-	Redirect,
+  BrowserRouter as Router,
+  Route,
+  Switch,
+  Redirect,
 } from 'react-router-dom';
 
 // App Component
 export default function App() {
-	return (
-		<ContextProvide>
-			<Router>
-				<Switch>
-					<Route exact path='/material-ui' component={MaterialUI} />
-					<Route exact path='/AntD' component={AntD} />
-					<Redirect to='/material-ui'></Redirect>
-				</Switch>
-			</Router>
-		</ContextProvide>
-	);
+  return (
+    <ContextProvide>
+      <Router>
+        <Switch>
+          <Route exact path='/material-ui' component={MaterialUI} />
+          <Route exact path='/AntD' component={AntD} />
+          <Redirect to='/material-ui'></Redirect>
+        </Switch>
+      </Router>
+    </ContextProvide>
+  );
 }
